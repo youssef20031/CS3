5b442a699de0a19b83448d0df8c395c9d56c22d0
youssefmaged <youssef.maged.6000@gmail.om>
2022-10-07 22:39:28 +0200

    Studying

--- studying/src/e2_9.java
+++ studying/src/e2_9.java
@@ -1,2 +1,25 @@
+import java.util.Random;
+
 public class e2_9 {
+    public static boolean isSorted(int [] a){
+        for(int i = 0; i<a.length-1;i++){
+            if(a[i]>a[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void shuffle(int [] a){
+        int t;
+        int f = (int) (Math.random()*a.length);
+        int k = (int) (Math.random()*a.length);
+        t = a[f];
+        a[f] = a[k];
+        a[k] = t;
+    }
+    public static void BogoSort(int [] a){
+        while (!isSorted(a)){
+            shuffle(a);
+        }
+    }
 }
