b20a6ccad194b7a31eb21f996a7541a8b4e81ec2
youss <youssef.maged.6000@gmail.com>
2022-11-02 23:25:33 +0200

    tt

--- Lab-5/src/e5_9.java
+++ Lab-5/src/e5_9.java
@@ -4,6 +4,15 @@ public class e5_9 {
     private int rear;
     private int nItems;
     private Object [] elements;
+    private int top;
+
+    public e5_9(int s) {
+        maxsize = s;
+        front = 0;
+        rear = -1;
+        elements = new Object[maxsize];
+        top = -1;
+    }
 
     public void enqueue(Object x)
     {
@@ -12,11 +21,13 @@ public class e5_9 {
 
         elements[++rear] = x;
         nItems++;
+
     }
     public Object dequeue()
     {
         Object result = elements[front];
         front++;
+        top++;
 
         if(front == maxsize)
             front = 0;
@@ -27,13 +38,36 @@ public class e5_9 {
     public void push(Object elem)
     {
         top++;
-        theStack[top] = elem;
+        front++;
+        elements[top] = elem;
+        nItems++;
     }
 
     public Object pop()
     {
-        Object result = theStack[top];
+        Object result = elements[top];
         top--;
+        nItems--;
         return result;
     }
+    public Object peek()
+    {
+        return elements[front];
+    }
+
+    public boolean isEmpty()
+    {
+        return (nItems == 0);
+    }
+
+    public boolean isFull()
+    {
+        return (nItems == maxsize);
+    }
+
+    public int size()
+    {
+        return nItems;
+    }
+
 }
