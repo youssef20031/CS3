92ea67536424005261abef35bc70f5d9a914c93b
youss <youssef.maged.6000@gmail.com>
2022-12-20 17:22:01 +0200

    ss

--- Lab-10/src/Main.java
+++ Lab-10/src/Main.java
@@ -1,19 +1,16 @@
-
 public class Main {
-
     public static void main(String[] args) {
         BTree a = new BTree();
         a.add(9);
         a.add(3);
         a.add(7);
         a.add(4);
-
-        BTree b = new BTree();
-        b.add(9);
-        b.add(3);
-        b.add(8);
-        b.add(4);
+        a.add(10);
+        a.add(20);
+        a.add(8);
+        a.add(1);
 
         a.displayTree();
+        a.breadthTraversal();
     }
 }
